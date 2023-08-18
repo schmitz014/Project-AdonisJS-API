e02dcf1d55cfe530d90a6ee405f36a2445343544
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-08-18 08:05:11 -0300

    feat: insering new data in DB

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,9 +1,18 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import Moment from 'App/Models/Moment'
 
 export default class MomentsController {
-  public async store() {
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.body()
+
+    const moment = await Moment.create(body)
+
+    response.status(201)
+
     return {
-      msg: 'It works!',
+      message: 'Moment has been created successfully',
+      data: moment,
     }
   }
 }
