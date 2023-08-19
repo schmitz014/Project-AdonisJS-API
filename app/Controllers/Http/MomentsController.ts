446719e2b1660f470638791fc18348676a3ce1d4
Pedro Schmitz <88664287+schmitz014@users.noreply.github.com>
2023-08-19 12:06:51 -0300

    fix and feat: fixing params and adding the complete CRUD for the project

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,13 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
 
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Moment from 'App/Models/Moment'
 
 import Application from '@ioc:Adonis/Core/Application'
 
 export default class MomentsController {
-  private validatioOptions = {
+  private validationOptions = {
     types: ['image'],
     size: '2mb',
   }
@@ -15,7 +15,7 @@ export default class MomentsController {
   public async store({ request, response }: HttpContextContract) {
     const body = request.body()
 
-    const image = request.file('image', this.validatioOptions)
+    const image = request.file('image', this.validationOptions)
 
     if (image) {
       const imageName = `${uuidv4()}.${image.extname}`
@@ -32,7 +32,7 @@ export default class MomentsController {
     response.status(201)
 
     return {
-      message: 'Moment has been created successfully',
+      message: 'Moment has been created successfully!',
       data: moment,
     }
   }
@@ -42,6 +42,55 @@ export default class MomentsController {
 
     return {
       data: moments,
+    }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const moment = await Moment.findOrFail(params.id)
+
+    return {
+      data: moment,
+    }
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const moment = await Moment.findOrFail(params.id)
+
+    await moment.delete()
+
+    return {
+      message: 'Moment deleted successfully!',
+      data: moment,
+    }
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const body = request.body()
+
+    const moment = await Moment.findOrFail(params.id)
+
+    moment.title = body.title
+    moment.description = body.description
+
+    if (moment.image !== body.image || !moment.image) {
+      const image = request.file('image', this.validationOptions)
+
+      if (image) {
+        const imageName = `${uuidv4()}.${image.extname}`
+
+        await image.move(Application.tmpPath('uploads'), {
+          name: imageName,
+        })
+
+        moment.image = imageName
+      }
+    }
+
+    await moment.save()
+
+    return {
+      message: 'Moment updated successfully!',
+      data: moment,
     }
   }
 }
